05bb08f60ccb733a18b2e5d8e2ed644435b8cae6
Yair Lavie <yair,lavie2008@gmail.com>
2024-04-19 12:02:04 +0300

    ClimbConstants and ClimbSubsystem
    i created some constants and added things to the ClimgSubsystem

--- src/main/java/frc/robot/Subsystem/ShooterArm/Climb/ClimbSubsystem.java
+++ src/main/java/frc/robot/Subsystem/ShooterArm/Climb/ClimbSubsystem.java
@@ -1,17 +1,59 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+/**this subsystem is responsible for climbing 
+ * @author: yair
+ * @Version 2.0.0
+*/
 
 package frc.robot.Subsystem.ShooterArm.Climb;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ClimbSubsystem extends SubsystemBase {
+
+public class ClimbSubsystem extends SubsystemBase implements ClimbConstants{
+  private CANSparkMax m_rightMotor;
+  private static ClimbSubsystem instance;
+  private CANSparkMax m_leftMotor;
+
+  public static ClimbSubsystem getInstance(){
+    if (instance == null)
+      instance = new ClimbSubsystem();
+    return instance;
+  }
   /** Creates a new ClimbSubsystem. */
-  public ClimbSubsystem() {}
+  private ClimbSubsystem() {
+    m_rightMotor = new CANSparkMax(CANSPARK_MAX_RIGHT_MOTOR_ID,  MotorType.kBrushless);
+    m_leftMotor = new CANSparkMax(CANSPARK_MAX_LEFT_MOTOR_ID,  MotorType.kBrushless);
+
+    m_rightMotor.setInverted(false);
+    m_leftMotor.setInverted(true);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+/**
+ * sets the speed for both motors
+ * @param speed is a value between -1 and 1 depanding on the the position of the arms
+ */
+  public void setSpeed(double speed) {
+    m_rightMotor.set(speed);
+    m_leftMotor.set(speed);
+  }
+/**
+ * sets the speed for the right motor
+ * @param speed is a value between -1 and 1 depanding on the the position of the arms
+ */
+  public void setRightSpeed(double speed) {
+    m_rightMotor.set(speed);
+  }
+/**
+ * sets the speed for the left motor
+ * @param speed is a value between -1 and 1 depanding on the the position of the arms
+ */
+  public void setLeftSpeed(double speed) {
+    m_leftMotor.set(speed);
   }
 }
